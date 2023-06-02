d8bbc15aed41c63e3afddd912075eaeed56bde47
Petar V Peshev <40143832+PetarVPeshev@users.noreply.github.com>
2023-06-02 15:41:46 +0200

    refactored code

--- superstrate_propagation_constant.m
+++ superstrate_propagation_constant.m
@@ -10,8 +10,8 @@ addpath('../spectral-methods-library');
 c = physconst('LightSpeed');
 wave_impedance = 376.730313668;
 
-Ner = 102;
-Nf = 101;
+Ner = 1002;
+Nf = 1001;
 stratification.er = NaN(1, Ner);
 
 %% PARAMETERS
@@ -35,28 +35,50 @@ dipole.W = wave.wavelength / 20;
 theta = linspace(eps, pi / 2 - 0.1 * pi / 180, 100);
 phi = linspace(0, 2 * pi, 400);
 sph_grid = meshgrid_comb(theta, phi);
+
+%% ELEVATION
 z = R * cos(sph_grid(:, :, 1));
 
 krho_te = NaN(length(stratification.er), length(wave.f));
 krho_tm = NaN(length(stratification.er), length(wave.f));
 dir_broadside = NaN(length(stratification.er), length(wave.f));
+E = NaN( [size(sph_grid, 1, 2), 3, 3] );
+idx = 0;
 for er_idx = 1 : 1 : length(stratification.er)
     krho_norm = linspace(1, sqrt(stratification.er(er_idx)), N);
 
     for f_idx = 1 : 1 : length(wave.f)
+        %% TE1 AND TM1 PROPAGATION CONSTANTS
         krho = krho_norm * wave.k0(f_idx);
     
         [krho_te(er_idx, f_idx), krho_tm(er_idx, f_idx)] ...
             = find_krho(wave.k0(f_idx), krho, 'Superstrate', ...
             stratification.h, stratification.hs(er_idx, f_idx), stratification.er(er_idx));
         
+        %% WAVE VECTOR COMPONENTS
         [k_comp, ~] = wave_vector(1, wave.k0(f_idx), sph_grid);
         KRHO = sqrt(k_comp(:, :, 1) .^ 2 + k_comp(:, :, 2) .^ 2);
+
+        %% VOLTAGE AND CURRENT FIELDS OF STRATIFIED MEDIA
         [vte, ite, vtm, itm] = stratified_media(wave.k0(f_idx), KRHO, z, ...
             'Superstrate', stratification.h, stratification.hs(er_idx, f_idx), stratification.er(er_idx));
+        
+        %% SPECTRAL GREEN'S FUNCTIONS
         SGF = spectral_gf(1, wave.k0(f_idx), k_comp(:, :, 1), k_comp(:, :, 2), vtm, vte, itm, ite, 'E', 'M');
+        
+        %% MAGNETIC CURRENT DENSITY
         Mx = ft_current(wave.k0(f_idx), k_comp, dipole.W(f_idx), dipole.L(f_idx), 1, 'dipole', 'x');
+        
+        %% ELECTRIC FIELD
         E = farfield(wave.k0(f_idx), R, sph_grid, k_comp(:, :, 3), z, SGF, Mx);
+        if er_idx == Ner - 1
+            if wave.f(f_idx) == 9e9 || wave.f(f_idx) == 10e9 || wave.f(f_idx) == 11e9
+                E(:, :, :, idx) = E;
+                idx = idx + 1;
+            end
+        end
+        
+        %% DIRECTIVITY
         [dir, ~, ~] = directivity(1, E, sph_grid, R);
         dir_broadside(er_idx, f_idx) = dir(1, 1);
     end
