18cef548e83db25c38802846d167a0ba666659de
Petar V Peshev <40143832+PetarVPeshev@users.noreply.github.com>
2023-06-10 16:46:39 +0200

    added functions for field calculations of multiple frequencies

--- superstrate_bandwidth.m
+++ superstrate_bandwidth.m
@@ -12,16 +12,22 @@ c = physconst('LightSpeed');
 wave_impedance = 376.730313668;
 
 %% PARAMETERS
-wave.f = linspace(9, 11, 101) * 1e9;
+% Wave parameters
+wave.f = linspace(8, 12, 101) * 1e9;
+% Stratification parameters
 stratification.h = 15 * 1e-3;
-stratification.er = 1 : 0.5 : 25;
+stratification.er = linspace(1, 25, 121);
+% Coordinate system parameters
 N = 1001;
 R = 1;
 
 %% DEPENDENT PARAMETERS
+% Wave dependent parameters
 wave.wavelength = c ./ wave.f;
 wave.k0 = 2 * pi ./ wave.wavelength;
+% Stratification dependent parameters
 stratification.hs = wave.wavelength ./ (4 * sqrt(stratification.er'));
+% Dipole dependent parameters
 dipole.L = wave.wavelength / 2;
 dipole.W = wave.wavelength / 20;
 
@@ -34,53 +40,47 @@ sph_grid = meshgrid_comb(theta, phi);
 z = R * cos(sph_grid(:, :, 1));
 
 %% WAVE VECTOR COMPONENTS
-kx = permute(wave.k0, [3 1 2]) .* sin(sph_grid(:, :, 1)) .* cos(sph_grid(:, :, 2));
-ky = permute(wave.k0, [3 1 2]) .* sin(sph_grid(:, :, 1)) .* sin(sph_grid(:, :, 2));
+[k_comp, ~, kx, ky, kz] = wave_vector_multi_freq(1, wave.k0, sph_grid);
 krho = sqrt(kx .^ 2 + ky .^ 2);
-kz = - 1j * sqrt(- permute(repmat(wave.k0', 1, 400, 100), [2 3 1]) .^ 2 + krho .^ 2);
+        
+%% MAGNETIC CURRENT DENSITY
+Mx = ft_current_multi_freq(wave.k0, k_comp, dipole.W, dipole.L, 1, ...
+    'dipole', 'x');
 
-%% DIRECTIVITY AND BANDWIDTH
-krho_te = NaN(length(stratification.er), length(wave.f));
-krho_tm = NaN(length(stratification.er), length(wave.f));
 dir_broadside = NaN(length(stratification.er), length(wave.f));
 fh = NaN(1, length(stratification.er));
 fl = NaN(1, length(stratification.er));
 for er_idx = 1 : 1 : length(stratification.er)
-    for f_idx = 1 : 1 : length(wave.f)
-        %% VOLTAGE AND CURRENT FIELDS OF STRATIFIED MEDIA
-        [vte, ite, vtm, itm] = stratified_media(wave.k0(f_idx), krho(:, :, f_idx), z, ...
-            'Superstrate', stratification.h, stratification.hs(er_idx, f_idx), stratification.er(er_idx));
-        
-        %% SPECTRAL GREEN'S FUNCTIONS
-        SGF = spectral_gf(1, wave.k0(f_idx), kx(:, :, f_idx), ky(:, :, f_idx), vtm, vte, itm, ite, 'E', 'M');
-        
-        %% MAGNETIC CURRENT DENSITY
-        k_comp = NaN( [size(sph_grid, 1, 2), 3] );
-        k_comp(:, :, 1) = kx(:, :, f_idx);
-        k_comp(:, :, 2) = ky(:, :, f_idx);
-        Mx = ft_current(wave.k0(f_idx), k_comp, dipole.W(f_idx), dipole.L(f_idx), 1, 'dipole', 'x');
-        
-        %% ELECTRIC FIELD
-        E = farfield(wave.k0(f_idx), R, sph_grid, kz(:, :, f_idx), z, SGF, Mx);
-        
-        %% DIRECTIVITY
-        [dir, ~, ~] = directivity(1, E, sph_grid, R);
-        dir_broadside(er_idx, f_idx) = dir(1, 1);
-    end
+    %% VOLTAGE AND CURRENT FIELDS OF STRATIFIED MEDIA
+    [vte, ite, vtm, itm] ...
+        = stratified_media_multi_freq(wave.k0, krho, z, ...
+        'Superstrate', stratification.h, stratification.hs(er_idx, :), ...
+        stratification.er(er_idx));
+            
+    %% SPECTRAL GREEN'S FUNCTIONS
+    SGF = spectral_gf_multi_freq(1, wave.k0, kx, ky, vtm, vte, itm, ite, ...
+        'E', 'M');
+            
+    %% ELECTRIC FIELD
+    E = farfield_multi_freq(wave.k0, R, sph_grid, kz, z, SGF, Mx);
+            
+    %% DIRECTIVITY
+    dir_broadside(er_idx, :) = broadside_directivity(1, E, sph_grid, R);
 
-    %% RESONANCE FREQUENCY
-    [peak, peak_idx_temp] = findpeaks(round(dir_broadside(er_idx, :), 4));
+    %% -3 dB POINTS
+    [peak, peak_idx] = max(dir_broadside(er_idx, :));
     if ~isempty(peak)
-        %% LOW AND HIGH FREQUENCY POINTS
-        % Low frequency cut
-        fl_temp = find(dir_broadside(er_idx, 1 : peak_idx_temp) ...
-            < peak - 3, 1, 'last');
+        % Low frequency point
+        fl_temp ...
+            = find(dir_broadside(er_idx, 1 : peak_idx) < (peak / 2), ...
+            1, 'last');
         if ~isempty(fl_temp)
             fl(er_idx) = wave.f(fl_temp);
         end
-        % High frequency cut
-        fh_temp = find(dir_broadside(er_idx, peak_idx_temp : end) ...
-            < peak - 3, 1) + peak_idx_temp - 1;
+        % High frequency point
+        fh_temp ...
+            = find(dir_broadside(er_idx, peak_idx : end) < (peak / 2), ...
+            1) + peak_idx - 1;
         if ~isempty(fh_temp)
             fh(er_idx) = wave.f(fh_temp);
         end
@@ -92,7 +92,7 @@ BW = 200 * (fh - fl) ./ (fh + fl);
 
 %% PLOT DIRECTIVITY
 figure('Position', [250 250 750 400]);
-for idx = 1 : 2 : length(stratification.er)
+for idx = 1 : 20 : length(stratification.er)
     plot(wave.f * 1e-9, 10 * log10(dir_broadside(idx, :)), ...
         'LineWidth', 2.0, 'DisplayName', ...
         ['dir, \epsilon_{r} = ' num2str(stratification.er(idx))]);
@@ -104,19 +104,20 @@ legend show;
 legend('location', 'bestoutside');
 xlabel('f / GHz');
 ylabel('D(\theta=0,\phi=0) / dB');
-title(['Broadside Directivity @ Superstrate, h = ' ...
-    num2str(stratification.h * 1e3) ' mm, ' ...
+title(['Broadside Directivity @ Superstrate, ' ...
+    num2str(stratification.h * 1e3) ' mm, and ' ...
     'h_{s} = \lambda_{0} / (4 * sqrt(\epsilon_{r}))']);
-saveas(gcf, 'figures\superstrate_dir_dedicated.fig');
+saveas(gcf, 'figures\superstrate_dir.fig');
 
 %% PLOT BANDWIDTH
 figure('Position', [250 250 750 400]);
 plot(stratification.er, BW, 'LineWidth', 2.0, 'DisplayName', 'BW');
 grid on;
-% ylim([0 13.5]);
+ylim([0 29]);
 legend show;
 legend('location', 'bestoutside');
 xlabel('\epsilon_{r}');
 ylabel('BW / %');
-title('Bandwidth @ Superstrate');
-saveas(gcf, 'figures\superstrate_bw_dedicated.fig');
+title(['Bandwidth @ Superstrate, h = ' num2str(stratification.h * 1e3) ...
+    ' mm']);
+saveas(gcf, 'figures\superstrate_bw.fig');
